bbd6be30976c613da5be4c7b1dd3b6b5cbf1013a
TensaCoder <herschel.menezes00@gmail.com>
2024-04-12 14:24:44 +0530

    DataForm CSS added for width and background added with its CSS

--- src/Pages/DataForm/DataForm.jsx
+++ src/Pages/DataForm/DataForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { MdDelete } from "react-icons/md";
 import { IoMdAddCircleOutline } from "react-icons/io";
+import "./DataForm.css";
 
 function MultiValueInput({ placeholder, onAdd }) {
   const [value, setValue] = useState("");
@@ -52,6 +53,8 @@ function DataForm() {
   const [employmentDuration, setEmploymentDuration] = useState("");
   const [employmentAreas, setEmploymentAreas] = useState([]);
   const [recommendations, setRecommendations] = useState("");
+  const [selectedBadges, setSelectedBadges] = useState([]);
+  const [badgesValidation, setBadgesValidation] = useState(true);
 
   const handleCourseAdd = (course) => {
     setCourses([...courses, course]);
@@ -65,14 +68,36 @@ function DataForm() {
     setEmploymentAreas([...employmentAreas, area]);
   };
 
+  const handleBadgeAdd = (badge) => {
+    setSelectedBadges([...selectedBadges, badge]);
+    if (selectedBadges.length === 0) {
+      setBadgesValidation(true);
+    }
+  };
+
+  const checkValidation = () =>{
+
+    
+
+    // Validate badges
+    if (selectedBadges.length === 0) {
+      // alert("Please select at least one badge.");
+      setBadgesValidation(false);
+      return;
+    }
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    // Call checkValidation
+
     // Handle form submission
     console.log("Form submitted:", {
       id,
       name,
       dob,
       involvement,
+      selectedBadges,
       courses,
       internshipDuration,
       internshipDomains,
@@ -84,125 +109,89 @@ function DataForm() {
 
   return (
     <>
-      <div className="container my-5 bg-dark text-light d-flex justify-content-center w-50">
-        <form onSubmit={handleSubmit} className="w-75">
-          <div className="form-row">
-            <div className="form-group my-3">
-              <label htmlFor="inputID" className="ms-1">
-                ID
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="inputID"
-                placeholder="ID"
-                value={id}
-                onChange={(e) => setId(e.target.value)}
-                maxLength="10"
-                required
-              />
-            </div>
-            <div className="form-group my-3">
-              <label htmlFor="inputName" className="ms-1">Name</label>
-              <input
-                type="text"
-                className="form-control"
-                id="inputName"
-                placeholder="Name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-              />
-            </div>
-          </div>
-          <div className="form-row my-3">
-            <div className="form-group my-3">
-              <label htmlFor="inputDOB" className="ms-1">Date of Birth</label>
-              <input
-                type="date"
-                className="form-control"
-                id="inputDOB"
-                value={dob}
-                onChange={(e) => setDob(e.target.value)}
-                required
-              />
-            </div>
-            <div className="form-group my-3">
-              <label htmlFor="inputInvolvement" className="ms-1">Involvement</label>
-              <select
-                id="inputInvolvement"
-                className="form-control"
-                value={involvement}
-                onChange={(e) => setInvolvement(e.target.value)}
-                required
-              >
-                <option value="">Choose...</option>
-                <option>Course</option>
-                <option>Internship</option>
-                <option>Employee</option>
-                <option>All of these</option>
-              </select>
-            </div>
-          </div>
-          <div className="form-group my-3">
-            <label htmlFor="inputCourses" className="ms-1">Courses Completed</label>
-            <MultiValueInput placeholder="Course" onAdd={handleCourseAdd} />
-            {courses.map((course, index) => (
-              <div key={index} className="d-flex align-items-center my-3">
+      <div className="main-cover py-5">
+        <div className="container text-light d-flex justify-content-center glass scrollable-div w-50">
+          <form onSubmit={handleSubmit} className="w-75">
+            <div className="row">
+              <div className="form-group my-3 col">
+                <label htmlFor="inputID" className="ms-1">
+                  ID
+                </label>
                 <input
                   type="text"
-                  className="form-control me-2"
-                  value={course}
-                  readOnly
+                  className="form-control"
+                  id="inputID"
+                  placeholder="ID"
+                  value={id}
+                  onChange={(e) => setId(e.target.value)}
+                  maxLength="10"
+                  required
                 />
-                <button
-                  type="button"
-                  className="btn btn-danger"
-                  style={{
-                    "--bs-btn-font-size": " 1.3rem",
-                    "--bs-btn-padding-x": "0.25rem",
-                    "--bs-btn-padding-y": "0.20rem",
-                  }}
-                  onClick={() =>
-                    setCourses(courses.filter((_, i) => i !== index))
-                  }
-                >
-                  <MdDelete />
-                </button>
               </div>
-            ))}
-          </div>
-          <div className="form-row my-3">
-            <div className="form-group my-3">
-              <label htmlFor="inputInternshipDuration" className="ms-1">
-                Internship Duration (months)
-              </label>
-              <input
-                type="number"
-                className="form-control"
-                id="inputInternshipDuration"
-                placeholder="Duration"
-                value={internshipDuration}
-                onChange={(e) => setInternshipDuration(e.target.value)}
-                min="1"
-              />
+              <div className="form-group my-3 col">
+                <label htmlFor="inputName" className="ms-1">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="inputName"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </div>
+            </div>
+            <div className="row my-2">
+              <div className="form-group col">
+                <label htmlFor="inputDOB" className="ms-1">
+                  Date of Birth
+                </label>
+                <input
+                  type="date"
+                  className="form-control"
+                  id="inputDOB"
+                  value={dob}
+                  onChange={(e) => setDob(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="form-group col">
+                <label htmlFor="inputInvolvement" className="ms-1">
+                  Involvement
+                </label>
+                <select
+                  id="inputInvolvement"
+                  className="form-control"
+                  value={involvement}
+                  onChange={(e) => setInvolvement(e.target.value)}
+                  required
+                >
+                  <option value="">Choose...</option>
+                  <option>Course</option>
+                  <option>Internship</option>
+                  <option>Employee</option>
+                  <option>All of these</option>
+                </select>
+              </div>
             </div>
             <div className="form-group my-3">
-              <label htmlFor="inputInternshipDomains" className="ms-1">
-                Internship Areas / Domains
+              <label htmlFor="inputBadges" className="ms-1">
+                Badges Achieved
               </label>
-
-              <MultiValueInput
-                placeholder="Domain"
-                onAdd={handleInternshipDomainAdd}
-              />
-
-              {internshipDomains.map((domain, index) => (
-                <div key={index} className="d-flex align-items-center my-2">
+              <MultiValueInput placeholder="Badge" onAdd={handleBadgeAdd} />
+              {badgesValidation ? (
+                <p></p>
+              ) : (
+                <span className="text-danger">At least one badge is needed</span>
+              )}
+              {selectedBadges.map((badge, index) => (
+                <div key={index} className="d-flex align-items-center my-3">
                   <input
                     type="text"
                     className="form-control me-2"
-                    value={domain}
+                    value={badge}
                     readOnly
                   />
                   <button
@@ -214,8 +203,8 @@ function DataForm() {
                       "--bs-btn-padding-y": "0.20rem",
                     }}
                     onClick={() =>
-                      setInternshipDomains(
-                        internshipDomains.filter((_, i) => i !== index)
+                      setSelectedBadges(
+                        selectedBadges.filter((_, i) => i !== index)
                       )
                     }
                   >
@@ -224,36 +213,17 @@ function DataForm() {
                 </div>
               ))}
             </div>
-          </div>
-          <div className="form-row my-3">
             <div className="form-group my-3">
-              <label htmlFor="inputEmploymentDuration" className="ms-1">
-                Employment Duration (months)
+              <label htmlFor="inputCourses" className="ms-1">
+                Courses Completed
               </label>
-              <input
-                type="number"
-                className="form-control"
-                id="inputEmploymentDuration"
-                placeholder="Duration"
-                value={employmentDuration}
-                onChange={(e) => setEmploymentDuration(e.target.value)}
-                min="1"
-              />
-            </div>
-            <div className="form-group my-3">
-              <label htmlFor="inputEmploymentAreas" className="ms-1">
-                Employment Areas and Duration
-              </label>
-              <MultiValueInput
-                placeholder="Area and Duration"
-                onAdd={handleEmploymentAreaAdd}
-              />
-              {employmentAreas.map((area, index) => (
-                <div key={index} className="d-flex align-items-center my-2">
+              <MultiValueInput placeholder="Course" onAdd={handleCourseAdd} />
+              {courses.map((course, index) => (
+                <div key={index} className="d-flex align-items-center my-3">
                   <input
                     type="text"
                     className="form-control me-2"
-                    value={area}
+                    value={course}
                     readOnly
                   />
                   <button
@@ -265,9 +235,7 @@ function DataForm() {
                       "--bs-btn-padding-y": "0.20rem",
                     }}
                     onClick={() =>
-                      setEmploymentAreas(
-                        employmentAreas.filter((_, i) => i !== index)
-                      )
+                      setCourses(courses.filter((_, i) => i !== index))
                     }
                   >
                     <MdDelete />
@@ -275,23 +243,130 @@ function DataForm() {
                 </div>
               ))}
             </div>
-          </div>
-          <div className="form-group my-3">
-            <label htmlFor="inputRecommendations" className="ms-1">Recommendations</label>
-            <textarea
-              className="form-control"
-              id="inputRecommendations"
-              rows="3"
-              value={recommendations}
-              onChange={(e) => setRecommendations(e.target.value)}
-            ></textarea>
-          </div>
-          <div className="d-flex justify-content-center my-5">
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </div>
-        </form>
+            <div className="row mt-4">
+              <div className="form-group col-6">
+                <label htmlFor="inputInternshipDuration" className="ms-1">
+                  Internship Duration (m)
+                </label>
+                <input
+                  type="number"
+                  className="form-control"
+                  id="inputInternshipDuration"
+                  placeholder="Duration"
+                  value={internshipDuration}
+                  onChange={(e) => setInternshipDuration(e.target.value)}
+                  min="1"
+                />
+              </div>
+              <div className="form-group col-6">
+                <label htmlFor="inputInternshipDomains" className="ms-1">
+                  Internship Areas / Domains
+                </label>
+
+                <MultiValueInput
+                  placeholder="Domain"
+                  onAdd={handleInternshipDomainAdd}
+                />
+
+                {internshipDomains.map((domain, index) => (
+                  <div key={index} className="d-flex align-items-center my-2">
+                    <input
+                      type="text"
+                      className="form-control me-2"
+                      value={domain}
+                      readOnly
+                    />
+                    <button
+                      type="button"
+                      className="btn btn-danger"
+                      style={{
+                        "--bs-btn-font-size": " 1.3rem",
+                        "--bs-btn-padding-x": "0.25rem",
+                        "--bs-btn-padding-y": "0.20rem",
+                      }}
+                      onClick={() =>
+                        setInternshipDomains(
+                          internshipDomains.filter((_, i) => i !== index)
+                        )
+                      }
+                    >
+                      <MdDelete />
+                    </button>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="row my-3">
+              <div className="form-group col">
+                <label htmlFor="inputEmploymentDuration" className="ms-1">
+                  Employment Duration (m)
+                </label>
+                <input
+                  type="number"
+                  className="form-control"
+                  id="inputEmploymentDuration"
+                  placeholder="Duration"
+                  value={employmentDuration}
+                  onChange={(e) => setEmploymentDuration(e.target.value)}
+                  min="1"
+                />
+              </div>
+              <div className="form-group col">
+                <label htmlFor="inputEmploymentAreas" className="ms-1">
+                  Employment Areas
+                </label>
+                <MultiValueInput
+                  placeholder="Area"
+                  onAdd={handleEmploymentAreaAdd}
+                />
+                {employmentAreas.map((area, index) => (
+                  <div key={index} className="d-flex align-items-center my-2">
+                    <input
+                      type="text"
+                      className="form-control me-2"
+                      value={area}
+                      readOnly
+                    />
+                    <button
+                      type="button"
+                      className="btn btn-danger"
+                      style={{
+                        "--bs-btn-font-size": " 1.3rem",
+                        "--bs-btn-padding-x": "0.25rem",
+                        "--bs-btn-padding-y": "0.15rem",
+                      }}
+                      onClick={() =>
+                        setEmploymentAreas(
+                          employmentAreas.filter((_, i) => i !== index)
+                        )
+                      }
+                    >
+                      <MdDelete />
+                    </button>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="form-group my-3">
+              <label htmlFor="inputRecommendations" className="ms-1">
+                Recommendations
+              </label>
+              <textarea
+                className="form-control"
+                id="inputRecommendations"
+                rows="3"
+                value={recommendations}
+                onChange={(e) => setRecommendations(e.target.value)}
+              ></textarea>
+            </div>
+
+            <div className="d-flex justify-content-center my-5">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
