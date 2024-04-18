65d90e521b5388a0343a5fbdfa6ae6dd0787635e
Bhavya <bhavyasura12@gmail.com>
2024-04-18 17:41:17 +0530

    Edit API

--- Backend/index.js
+++ Backend/index.js
@@ -21,6 +21,46 @@ app.post('/data-entry/saveData', async (req, res) => {
     }
 });
 
+app.get('/data-entry/editData', async (req, res) => {
+    const { name } = req.query;
+    try {
+        const user = await User.findOne({ name });
+        if (!user) {
+            return res.status(404).send({ message: 'User not found' });
+        }
+        console.log(user);
+        res.status(200).send(user);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+});
+
+app.put('/data-entry/editData/saveData', async (req, res) => {
+
+    // const { name, updatedUserData } = req.body;
+    const { name } = req.query;
+    const updates = req.body;
+    try {
+        const user = await User.findOne({ name });
+        if (!user) {
+            return res.status(404).send({ message: 'User not found' });
+        }
+        Object.keys(updates).forEach((key) => {
+            user[key] = updates[key];
+        });
+        // updates.forEach((update) => {
+        //     user[update] = updatedUserData[update];
+        // });
+        await user.save();
+        console.log(user);
+        res.status(200).send(user);
+    } catch (error) {
+        console.log(error);
+        res.status(400).send(error);
+    }
+});
+
 const port = process.env.PORT || 6000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
